f55e8a24232f53aac27b027d5f2665ef03359162
oの字 <107860356+o-noji@users.noreply.github.com>
2023-12-06 12:58:33 +0900

    ⚡️

--- src/app/api/sendMail/route.js
+++ src/app/api/sendMail/route.js
@@ -1,15 +1,14 @@
-import sgMail from '@sendgrid/mail';
+import sgMail from "@sendgrid/mail";
 sgMail.setApiKey(process.env.SENDGRID_API);
 
 export default async function handler(req, res) {
-  let response1 = null;
-  let response2 = null;
+  let response = null;
 
-  if (req.method === 'POST') {
+  if (req.method === "POST") {
     const msg = {
       to: req.body.email,
       from: process.env.MAIL_FROM,
-      subject: 'お問合せありがとうございました。',
+      subject: "お問合せありがとうございました。",
       html: `
       <p>${req.body.name}様<br>お問合せを受け付けました。回答をお待ちください。</p>
       <p>【お名前】</p>
@@ -24,7 +23,7 @@ export default async function handler(req, res) {
     const notification = {
       to: process.env.MAIL_TO,
       from: process.env.MAIL_FROM,
-      subject: 'お問合せがありました。',
+      subject: "お問合せがありました。",
       html: `
       <p>【お名前】</p>
       <p>${req.body.name}</p>
@@ -36,14 +35,19 @@ export default async function handler(req, res) {
     };
 
     try {
-      response1 = await sgMail.send(msg);
-      response2 = await sgMail.send(notification);
-      res.status(200).json({ message: 'メールが送信されました。' });
+      const response1 = await sgMail.send(msg);
+      const response2 = await sgMail.send(notification);
+      response = {
+        message: "メールが送信されました。",
+        response1,
+        response2,
+      };
+      res.status(200).json(response);
     } catch (error) {
-      // eslint-disable-next-line no-console
-      console.error('メールの送信中にエラーが発生しました:', error);
+      console.error("メールの送信中にエラーが発生しました:", error);
+      res.status(500).json({ error: "内部サーバーエラー" });
     }
+  } else {
+    res.status(405).json({ error: "許可されていないメソッド" });
   }
-  res.send(response1);
-  res.send(response2);
 }
