b4513e6b04f41092e33a92fd706b7f1d1bd6ca3a
oの字 <107860356+o-noji@users.noreply.github.com>
2023-12-06 13:25:41 +0900

    ⚡️

--- src/app/api/sendMail/route.js
+++ src/app/api/sendMail/route.js
@@ -1,7 +1,7 @@
 import sgMail from "@sendgrid/mail";
 sgMail.setApiKey(process.env.SENDGRID_API);
 
-export default async function handler(req, res) {
+export const postSendMail = async (req, res) => {
   let response = null;
 
   if (req.method === "POST") {
@@ -50,4 +50,4 @@ export default async function handler(req, res) {
   } else {
     res.status(405).json({ error: "許可されていないメソッド" });
   }
-}
+};
